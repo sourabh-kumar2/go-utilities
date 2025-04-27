15fa8400e8b9fec38617feca6ccb236a0109a8cb
Sourabh Kumar <64460856+Sourabh-Kumar2@users.noreply.github.com>
2025-04-27 11:09:42 +0530

    Define WorkerPool struct to manage workers and task distribution.

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -1,5 +1,10 @@
 package workerpool
 
+import (
+	"context"
+	"sync"
+)
+
 // Task defines an interface for tasks to be executed by workers in the pool.
 type Task interface {
 	Execute() (interface{}, error) // Executes the task and returns the result or an error.
@@ -10,4 +15,14 @@ type Result struct {
 	Output interface{} // The output of the task execution.
 	Err    error       // Any error that occurred during the task execution.
 	Task   Task        // The task that was executed.
+}
+
+// WorkerPool manages a pool of workers and task processing.
+type WorkerPool struct {
+	workersCount int                // The number of workers in the pool.
+	tasksChan    chan Task          // Channel to send tasks to workers.
+	resultsChan  chan Result        // Channel to receive results from workers.
+	wg           sync.WaitGroup     // WaitGroup to wait for all workers to finish.
+	ctx          context.Context    // Context to control cancellation of worker operations.
+	cancel       context.CancelFunc // Function to cancel the context and stop the workers.
 }
